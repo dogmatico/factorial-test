d25725041a7714b30866f120c9d5dd61376cee83
Cristian Lorenzo i Martínez <cristian@epsilon-delta.eu>
2025-03-30 14:17:04 +0200

    feat(base-api-interfaces): externalize API interfaces

---
+++ packages/base-api-interfaces/src/interfaces.ts
@@ -0,0 +1,23 @@
+export interface APIError {
+	/**
+	 * Code for programmatic handling
+	 */
+	code: string;
+
+	/**
+	 * Human readable description
+	 */
+	description: string;
+
+	/**
+	 * UUID for helpdesk/debug
+	 */
+	id?: string;
+}
+
+export interface APIResponse<T> {
+	isSuccess: boolean;
+	data: T | null;
+	errors: APIError[];
+	warnings: APIError[];
+}
